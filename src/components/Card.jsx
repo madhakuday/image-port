75986a5de0dcd02e7a27c8ece1860d00c53256d3
Hardik Mehta <hadzy1802@gmail.com>
2022-12-18 22:32:18 +0530

    do some changes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,25 +1,8 @@
 import React from "react";
-import img1 from "../Assetes/New folder/img-1.png";
-import img2 from "../Assetes/New folder/img-2.png";
-import img3 from "../Assetes/New folder/img-3.png";
-import img4 from "../Assetes/New folder/img-4.png";
-import img5 from "../Assetes/New folder/img-5.png";
-import img6 from "../Assetes/New folder/img-6.png";
-import img7 from "../Assetes/New folder/img-7.png";
-import img8 from "../Assetes/New folder/img-8.png";
 import { useNavigate } from "react-router";
-import "./Card.css";
 
-const cardData = [
-  { id: 1, img: img1 },
-  { id: 2, img: img2 },
-  { id: 3, img: img3 },
-  { id: 4, img: img4 },
-  { id: 5, img: img5 },
-  { id: 6, img: img6 },
-  { id: 7, img: img7 },
-  { id: 8, img: img8 },
-];
+import "./Card.css";
+import cardData from "../data/cardData";
 
 const Card = () => {
   const navigate = useNavigate();
--- src/components/SingalCard.jsx
+++ src/components/SingalCard.jsx
@@ -1,25 +1,6 @@
 import React, { useEffect, useState } from "react";
-import img1 from "../Assetes/New folder/img-1.png";
-import img2 from "../Assetes/New folder/img-2.png";
-import img3 from "../Assetes/New folder/img-3.png";
-import img4 from "../Assetes/New folder/img-4.png";
-import img5 from "../Assetes/New folder/img-5.png";
-import img6 from "../Assetes/New folder/img-6.png";
-import img7 from "../Assetes/New folder/img-7.png";
-import img8 from "../Assetes/New folder/img-8.png";
-import { useLocation, useNavigate, useParams } from "react-router";
-import "./Card.css";
-
-const cardData = [
-  { id: 1, img: img1 },
-  { id: 2, img: img2 },
-  { id: 3, img: img3 },
-  { id: 4, img: img4 },
-  { id: 5, img: img5 },
-  { id: 6, img: img6 },
-  { id: 7, img: img7 },
-  { id: 8, img: img8 },
-];
+import { useNavigate, useParams } from "react-router";
+import cardData from "../data/cardData";
 
 const SingalCard = () => {
   const { id } = useParams();
@@ -50,11 +31,11 @@ const SingalCard = () => {
               width: "100%",
               display: "flex",
               alignItems: "center",
-              height: "100vh",
+              height: "100%",
               justifyContent: "center",
             }}
           >
-            <img src={x.img} alt="" width="50%" />
+            <img src={x.fImg} alt="" width="50%" />
           </div>
         </>
       ))}
