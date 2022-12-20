1fb72eb6e8373c0ec67b031498aeff6c89c5039f
Hardik Mehta <hadzy1802@gmail.com>
2022-12-20 22:14:00 +0530

    change some data

--- src/data/cardData.js
+++ src/data/cardData.js
@@ -2,21 +2,32 @@ import img1 from "../Assetes/image/img-1.png";
 import img2 from "../Assetes/image/img-2.png";
 import img3 from "../Assetes/image/img-3.png";
 import img4 from "../Assetes/image/img-4.png";
+import img5 from "../Assetes/image/img-5.png";
+import img6 from "../Assetes/image/img-6.png";
+import img7 from "../Assetes/image/img-7.png";
+import img8 from "../Assetes/image/img-8.png";
+import img9 from "../Assetes/image/img-9.png";
 
 /*Full page mg */
 import fImg1 from "../Assetes/fullpageimg/img-1.png";
 import fImg2 from "../Assetes/fullpageimg/img-2.png";
 import fImg3 from "../Assetes/fullpageimg/img-3.png";
 import fImg4 from "../Assetes/fullpageimg/img-4.png";
+import fImg5 from "../Assetes/fullpageimg/img-5.png";
+import fImg6 from "../Assetes/fullpageimg/img-6.png";
+import fImg7 from "../Assetes/fullpageimg/img-7.png";
+import fImg8 from "../Assetes/fullpageimg/img-8.png";
+import fImg9 from "../Assetes/fullpageimg/img-9.png";
 const cardData = [
   { id: 1, img: img1, fImg: fImg1 },
   { id: 2, img: img2, fImg: fImg2 },
   { id: 3, img: img3, fImg: fImg3 },
   { id: 4, img: img4, fImg: fImg4 },
-  // { id: 5, img: img5 },
-  // { id: 6, img: img6 },
-  // { id: 7, img: img7 },
-  // { id: 8, img: img8 },
+  { id: 5, img: img5, fImg: fImg5 },
+  { id: 6, img: img6, fImg: fImg6 },
+  { id: 7, img: img7, fImg: fImg7 },
+  { id: 8, img: img8, fImg: fImg8 },
+  { id: 9, img: img9, fImg: fImg9 },
 ];
 
 export default cardData;
